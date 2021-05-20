75f7f9d8ca4fddd4a7d7c14be45a617f8620bf40
Latisha Faroul <Lfaroul14@gmail.com>
2021-05-20 00:35:44 -0400

    commit tree 45736830d1ceff09f253acde3352091e307173d7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+function mondayWork(activity = "go to the office"){
+    return(`This Monday, I will ${activity}.`);
+}
+mondayWork();
+
+function wrapAdjective(){
+    //How to make '*' work using 'let'
+    function innerFunction(being="special"){
+        return (`You are ${being}`);
+    }
+}
+wrapAdjective();
